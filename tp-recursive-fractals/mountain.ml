dec142e3310ed35021286c98e39bc0f5fa8daba4
toogy <contact@toogy.fr>
2012-10-01 10:08:01 +0200

    things obvious in mountain.ml

--- tp-recursive-fractals/mountain.ml
+++ tp-recursive-fractals/mountain.ml
@@ -5,9 +5,8 @@ open_graph "";;
 let rec mountain x y z t = function
 	| 0 -> moveto x y; lineto z t
 	| n -> 
-		let h = (y+t)/2 + int(abs(z-x)/5 + 20) in
-			let u = x+(z-x)/2 and v = h in
-				mountain x y u v (n-1);
-				mountain u v z t (n-1);;
+		let h = (y+t)/2 + int(abs(z-x)/5 + 20) and u = x+(z-x)/2 in
+				mountain x y u h (n-1);
+				mountain u h z t (n-1);;
 
 mountain 20 20 500 20 8;;
